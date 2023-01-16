45b1a5558b8575b700ad5bf64927ba7ee3a4fdd3
Anewyn <Anewynn@gmail.com>
2023-01-16 19:34:50 +0100

    API emulador

--- app/src/main/java/com/alejandro/rio/view/MainActivity.java
+++ app/src/main/java/com/alejandro/rio/view/MainActivity.java
@@ -6,16 +6,21 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.alejandro.rio.R;
 import com.alejandro.rio.model.Guild;
 import com.alejandro.rio.model.PJ;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.ServerError;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
@@ -29,20 +34,18 @@ import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
     private RequestQueue requestQueue;
-    private ImageView profilePicture;
+    private ImageView profilePicture, achievementIcon, honorableKillsIcon, raceIcon, guildIcon;
     private TextView characterNameTV, characterClassTV, characterSpecTV, characterRoleTV, achievementPointsTV,
-            honorableKillsTV, guildNameTV, raceTV;
+            honorableKillsTV, guildNameTV, raceTV, classFieldTV, specFieldTV, roleFieldTV;
     BottomNavigationView bottomNavigationView;
-
-//    private PJ pj = new PJ();
+    private ProgressBar loadingPB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setup();
-//        sendGetRequests();
-//        setProfileData();
+        hideUI();
         jsonObjectWOWRequest();
     }
 
@@ -57,16 +60,62 @@ public class MainActivity extends AppCompatActivity {
         honorableKillsTV = findViewById(R.id.activity_main_honorable_kills);
         guildNameTV = findViewById(R.id.activity_main_character_guild);
         raceTV = findViewById(R.id.activity_main_character_race);
+        loadingPB = findViewById(R.id.main_activity_progress_bar);
+        classFieldTV = findViewById(R.id.activity_main_class_field);
+        specFieldTV = findViewById(R.id.activity_main_spec_field);
+        roleFieldTV = findViewById(R.id.activity_main_role_field);
+        achievementIcon = findViewById(R.id.activity_main_character_achievement_points_icon);
+        honorableKillsIcon = findViewById(R.id.activity_main_honorable_kills_icon);
+        raceIcon = findViewById(R.id.activity_main_character_race_icon);
+        guildIcon = findViewById(R.id.activity_main_character_guild_icon);
         requestQueue = Volley.newRequestQueue(this);
+    }
+
+    private void hideUI() {
+        profilePicture.setVisibility(View.GONE);
+        characterNameTV.setVisibility(View.GONE);
+        characterClassTV.setVisibility(View.GONE);
+        characterSpecTV.setVisibility(View.GONE);
+        characterRoleTV.setVisibility(View.GONE);
+        achievementPointsTV.setVisibility(View.GONE);
+        honorableKillsTV.setVisibility(View.GONE);
+        guildNameTV.setVisibility(View.GONE);
+        raceTV.setVisibility(View.GONE);
+        classFieldTV.setVisibility(View.GONE);
+        roleFieldTV.setVisibility(View.GONE);
+        specFieldTV.setVisibility(View.GONE);
+        achievementIcon.setVisibility(View.GONE);
+        honorableKillsIcon.setVisibility(View.GONE);
+        raceIcon.setVisibility(View.GONE);
+        guildIcon.setVisibility(View.GONE);
+    }
+
+    private void showUI() {
+        profilePicture.setVisibility(View.VISIBLE);
+        characterNameTV.setVisibility(View.VISIBLE);
+        characterClassTV.setVisibility(View.VISIBLE);
+        characterSpecTV.setVisibility(View.VISIBLE);
+        characterRoleTV.setVisibility(View.VISIBLE);
+        achievementPointsTV.setVisibility(View.VISIBLE);
+        honorableKillsTV.setVisibility(View.VISIBLE);
+        guildNameTV.setVisibility(View.VISIBLE);
+        raceTV.setVisibility(View.VISIBLE);
+        classFieldTV.setVisibility(View.VISIBLE);
+        roleFieldTV.setVisibility(View.VISIBLE);
+        specFieldTV.setVisibility(View.VISIBLE);
+        achievementIcon.setVisibility(View.VISIBLE);
+        honorableKillsIcon.setVisibility(View.VISIBLE);
+        raceIcon.setVisibility(View.VISIBLE);
+        guildIcon.setVisibility(View.VISIBLE);
     }
 
     private void jsonObjectWOWRequest() {
         final String URL_OBJECT = "https://raider.io/api/v1/characters/profile?region=eu&realm=sanguino&name=anewyn&fields=guild";
+        loadingPB.setVisibility(View.VISIBLE);
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, URL_OBJECT, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
+                response -> {
+                    try {
+                        if (response != null) {
                             String name = response.getString("name");
                             Log.d("RIO", name);
                             characterNameTV.setText(name);
@@ -89,43 +138,61 @@ public class MainActivity extends AppCompatActivity {
                             Log.d("RIO", honorableKills);
                             honorableKillsTV.setText(honorableKills);
                             String guildName = response.getJSONObject("guild").getString("name");
-//                            String guildName = response.getString("gender");
-                            Log.d("RIO",guildName);
+                            Log.d("RIO", guildName);
                             guildNameTV.setText(guildName);
                             imageRequest(response.getString("thumbnail_url"));
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+                            loadingPB.setVisibility(View.GONE);
+                            showUI();
                         }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                if (error.networkResponse.statusCode == 400) {
-                    Log.i("Volley", "Error 400");
-                    try {
-                        Log.i("API", new String(error.networkResponse.data, StandardCharsets.UTF_8));
-                        Toast.makeText(MainActivity.this, "No se ha podido encontrar el personaje en ese servidor", Toast.LENGTH_LONG).show();
-                    } catch (Exception e) {
+                    } catch (JSONException e) {
                         e.printStackTrace();
                     }
+                }, error -> {
+            if (error.networkResponse.statusCode == 400) {
+                Log.i("Volley", "Error 400");
+                try {
+                    Log.i("API", new String(error.networkResponse.data, StandardCharsets.UTF_8));
+                    Toast.makeText(MainActivity.this, "No se ha podido encontrar el personaje en ese servidor", Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
+            }
+            if (error instanceof ServerError) {
+                Log.i("Volley", "Error del servidor");
+                Toast.makeText(MainActivity.this, "Error del servidor", Toast.LENGTH_LONG).show();
+
+            }
+            if (error instanceof NoConnectionError) {
+                Log.i("Volley", "Error, no hay conexión a internet");
+                Toast.makeText(MainActivity.this, "Sin conexión a internet", Toast.LENGTH_LONG).show();
+            }
+            if (error instanceof NetworkError) { //Salta cuando no hay conexión a internet, cuando hay problemas de DNS o cuando falla el socket
+                Log.i("Volley", "Ha ocurrido un error de red, revisa la conexión a internet");
             }
         });
         requestQueue.add(jsonObjectRequest);
     }
+
     private void imageRequest(String imageURL) {
         ImageRequest imageRequest = new ImageRequest(imageURL,
-                new Response.Listener<Bitmap>() {
-                    @Override
-                    public void onResponse(Bitmap response) {
+                response -> {
+                    if (response != null) {
                         profilePicture.setImageBitmap(response);
-
                     }
                 }, 150, 150, null, null,
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
+                error -> {
+                    if (error instanceof ServerError) {
+                        Log.i("Volley", "Error del servidor");
+                        Toast.makeText(MainActivity.this, "Error del servidor", Toast.LENGTH_LONG).show();
 
+                    }
+                    if (error instanceof NoConnectionError) {
+                        Log.i("Volley", "Error, no hay conexión a internet");
+                        Toast.makeText(MainActivity.this, "Sin conexión a internet", Toast.LENGTH_LONG).show();
+                    }
+                    if (error instanceof NetworkError) { //Salta cuando no hay conexión a internet, cuando hay problemas de DNS o cuando falla el socket
+                        Log.i("Volley", "Ha ocurrido un error de red, revisa la conexión a internet");
+                        Toast.makeText(MainActivity.this, "Ha ocurrido un error de red, revisa la conexión a internet", Toast.LENGTH_LONG).show();
                     }
                 });
         requestQueue.add(imageRequest);
--- app/src/main/java/com/alejandro/rio/view/PostActivity.java
+++ app/src/main/java/com/alejandro/rio/view/PostActivity.java
@@ -13,10 +13,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.alejandro.rio.R;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
+import com.android.volley.ServerError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -29,7 +30,7 @@ import java.util.Map;
 
 public class PostActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
-    private EditText nameEdt, jobEdt;
+    private EditText usernameEdtText, passwordEdtText;
     private Button registerBtn;
     private TextView apiResponseTV;
     private ProgressBar loadingPB;
@@ -43,22 +44,23 @@ public class PostActivity extends AppCompatActivity {
 
     private void setup() {
         setUpNavBar();
-        nameEdt = findViewById(R.id.post_activity_content1);
-        jobEdt = findViewById(R.id.post_activity_content2);
+        usernameEdtText = findViewById(R.id.post_activity_content1);
+        passwordEdtText = findViewById(R.id.post_activity_content2);
         registerBtn = findViewById(R.id.post_activity_send_button);
         apiResponseTV = findViewById(R.id.post_activity_response_field);
+        apiResponseTV.setVisibility(View.GONE);
         loadingPB = findViewById(R.id.post_activity_progress_bar);
 
         registerBtn.setOnClickListener(v -> {
-            if (nameEdt.getText().toString().isBlank() || jobEdt.getText().toString().isBlank()) {
+            if (usernameEdtText.getText().toString().isBlank() || passwordEdtText.getText().toString().isBlank()) {
                 Toast.makeText(PostActivity.this, "No dejes nada en blanco", Toast.LENGTH_SHORT).show();
             } else {
-                sendPostRequest(nameEdt.getText().toString().trim(), jobEdt.getText().toString().trim());
+                sendPostRequest(usernameEdtText.getText().toString().trim(), passwordEdtText.getText().toString().trim());
             }
         });
     }
 
-    private void sendPostRequest(String name, String job) {
+    private void sendPostRequest(String name, String password) {
         // https://reqres.in/
         final String URL = "https://my-json-server.typicode.com/typicode/demo/posts";
         loadingPB.setVisibility(View.VISIBLE);
@@ -66,34 +68,45 @@ public class PostActivity extends AppCompatActivity {
         RequestQueue requestQueue = Volley.newRequestQueue(PostActivity.this);
 
         StringRequest stringRequest = new StringRequest(Request.Method.POST, URL,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
+                response -> {
+                    if (response != null) {
                         loadingPB.setVisibility(View.GONE);
-                        nameEdt.setText("");
-                        jobEdt.setText("");
+                        usernameEdtText.setText("");
+                        passwordEdtText.setText("");
                         Toast.makeText(PostActivity.this, "Usuario registrado", Toast.LENGTH_SHORT).show();
                         try {
                             JSONObject jsonObject = new JSONObject(response);
                             String responseUserNameId = jsonObject.getString("id");
-                            Log.d("REVOPOST",responseUserNameId);
-                            apiResponseTV.setText("Usuario registrado con ID "+responseUserNameId);
+                            Log.d("REVOPOST", responseUserNameId);
+                            apiResponseTV.setText("Usuario registrado con ID " + responseUserNameId);
+                            apiResponseTV.setVisibility(View.VISIBLE);
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("Volley POST", error.toString());
-                Toast.makeText(PostActivity.this, "Fallo al realizar la petición ", Toast.LENGTH_SHORT).show();
+                }, error -> {
+            Log.e("Volley POST", error.toString());
+            Toast.makeText(PostActivity.this, "Fallo al realizar la petición ", Toast.LENGTH_SHORT).show();
+            if (error instanceof ServerError) {
+                Log.i("Volley", "Error del servidor");
+                Toast.makeText(PostActivity.this, "Error del servidor", Toast.LENGTH_LONG).show();
+
+            }
+            if (error instanceof NoConnectionError) {
+                Log.i("Volley", "Error, no hay conexión a internet");
+                Toast.makeText(PostActivity.this, "Sin conexión a internet", Toast.LENGTH_LONG).show();
+            }
+            if (error instanceof NetworkError) { //Salta cuando no hay conexión a internet, cuando hay problemas de DNS o cuando falla el socket
+                Log.i("Volley", "Ha ocurrido un error de red, revisa la conexión a internet");
+                Toast.makeText(PostActivity.this, "Ha ocurrido un error de red, revisa la conexión a internet", Toast.LENGTH_LONG).show();
             }
         }) {
             @Override
-            protected Map<String, String> getParams(){
+            protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<>();
-                params.put("title",name);
-//                params.put("job",job);
+                params.put("username", name);
+                params.put("password", password);
                 return params;
             }
         };
